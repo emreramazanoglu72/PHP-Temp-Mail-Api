ea84264be087e0e6924365165bbddc9d14780c21
Emre Ramazanoğlu <73821776+emreramazanoglu72@users.noreply.github.com>
2020-12-01 23:50:15 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+    /**
+     * Import TempMail Api
+     */
+    require './dist/TempMail.php';
+
+    $tempmail = new TempMailApi();
+    /**
+     * Get Random Mail List
+     * $limit = default 10;
+     */
+        $result = $tempmail->getMail(10);
+    /**
+     * Get Connect Mail Session
+     * $session = mail name convert base64;
+     */
+    $result = $tempmail->getMailInfo ("Z3RkamxhQDFzZWNtYWlsLm9yZw==");
+    /**
+     * Get Mail Detail
+     * $session = mail name convert base64;
+     * $id = mesaj detail id
+     */
+    $result = $tempmail->MailDetail ("Z3RkamxhQDFzZWNtYWlsLm9yZw==",1212121);
+
+    /**
+     * File Download
+     * $session = mail name convert base64;
+     * $id = mesaj detail id
+     * $file = file Name
+     */
+    $result = $tempmail->fileDownload ("Z3RkamxhQDFzZWNtYWlsLm9yZw==",1212121,"myAbout.docs");
